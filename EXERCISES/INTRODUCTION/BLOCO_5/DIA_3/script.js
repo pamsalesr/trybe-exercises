bdc01222118a5ff92fe3be5d0809dfda2b188d56
Pamela Anderson Sales <pamelasalesr@gmail.com>
2020-12-07 15:33:56 -0300

    Finish day 3 exercises

--- EXERCISES/INTRODUCTION/BLOCO_5/DIA_3/script.js
+++ EXERCISES/INTRODUCTION/BLOCO_5/DIA_3/script.js
@@ -1,6 +1,7 @@
 function createDaysOfTheWeek() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
   const weekDaysList = document.querySelector('.week-days');
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   for (let index = 0; index < weekDays.length; index += 1) {
     const days = weekDays[index];
