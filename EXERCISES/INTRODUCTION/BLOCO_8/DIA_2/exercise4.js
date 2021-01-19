60d09096fcdbd5f000fb5a0e19ecc10f561e4c32
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-01-19 19:00:47 -0300

    Add exercise4.js

--- EXERCISES/INTRODUCTION/BLOCO_8/DIA_2/exercise4.js
+++ EXERCISES/INTRODUCTION/BLOCO_8/DIA_2/exercise4.js
@@ -64,54 +64,36 @@ const books = [
 ];
 
 const expectedResult = [
- {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948
-    },
-    releaseYear: 1991,
-  },
   {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928
   },
   {
     id: 3,
     name: 'Fundação',
     genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
   },
   {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  }
 ]
 
-// Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+// Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
 
-function fantasyOrScienceFiction() {
-  return books.filter(book => book.genre === 'Fantasia' || book.genre === 'Ficção Científica')
+function oldBooks() {
+  const currentYear = new Date().getFullYear();
+  return books.filter(book => currentYear - book.releaseYear > 60).sort((release1, release2) => release1.releaseYear - release2.releaseYear);
 }
 
-// console.log(fantasyOrScienceFiction());
+//console.log(oldBooks());
 
-assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
+assert.deepStrictEqual(oldBooks(), expectedResult);
