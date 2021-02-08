7cf873923c9badd634691d0dd8c4154406cd54fe
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-02-08 15:29:03 -0300

    Add day 2 exercises

--- EXERCISES/INTRODUCTION/BLOCO_10/DIA_2/exercises.test.js
+++ EXERCISES/INTRODUCTION/BLOCO_10/DIA_2/exercises.test.js
@@ -14,8 +14,7 @@ test('Test if uppercase function returns ANA MARIA', (done) => {
 })
 */
 
-// Código-base para os exercícios 2 e 3:
-
+/*
 const uppercase = (str, callback) => {
   callback(str.toUpperCase());
 }
@@ -26,7 +25,10 @@ it(`uppercase 'test' to equal 'TEST'`, (done) => {
     done();
   });
 });
+*/
 
+// Código-base para os exercícios 2 e 3:
+/*
 const users = {
   4: { name: 'Mark' },
   5: { name: 'Paul' }
@@ -45,6 +47,43 @@ const findUserById = (id) => {
 const getUserName = (userID) => {
   return findUserById(userID).then(user => user.name);
 };
-
+*/
 // EXERCICIO 2:
 // Utilizando a sintaxe de Promise, faça um teste que verifique o resultado da função getUserName para o caso em que o usuário é encontrado, e também um teste para o caso em que o usuário não é encontrado.
+/*
+describe('Qaundo o id existe', () => {
+  test('Retorne o nome do usuario', () => {
+    expect.assertions(1);
+    return getUserName(5).then((data) => expect(data).toEqual('Paul'));
+  });
+});
+
+describe('Qaundo o id nao existe', () => {
+  test('Retorna um erro', () => {
+    expect.assertions(1);
+    return getUserName(3).catch((error) => expect(error).toEqual({ error: 'User with 3 not found.' }));
+  });
+});
+*/
+
+// EXERCICIO 3: Reescreva o teste do exercício anterior, desta vez utilizando a sintaxe de async/await .
+/*
+describe('Qaundo o id existe - async/await', () => {
+  test('Retorne o nome do usuario', async () => {
+    expect.assertions(1);
+    const data = await getUserName(4);
+    expect(data).toEqual('Mark');
+  });
+});
+
+describe('Qaundo o id nao existe - async/await', () => {
+  test('Retorne um erro', async () => {
+    expect.assertions(1);
+    try {
+      await getUserName(1);
+    } catch (error) {
+      expect(error).toEqual({ error: 'User with 1 not found.' })
+    }
+  });
+});
+*/
